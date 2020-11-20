9da64fe64391ff034053a1a056f3497c4179d9b1
y0092888 <ju-horn@web.de>
2020-11-20 17:13:31 +0100

    todo app

---
+++ lib/screens/taskpage.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class Taskpage extends StatefulWidget {
+  @override
+  _TaskpageState createState() => _TaskpageState();
+}
+
+class _TaskpageState extends State<Taskpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+      child: Container(
+        child: Column(children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 24.0,
+            ),
+            child: Row(
+              children: [
+                InkWell(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(24.0),
+                    child: Image(
+                      image: AssetImage('assets/images/back_arrow_icon.png'),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: "Enter Task Title",
+                      border: InputBorder.none,
+                    ),
+                    style: TextStyle(
+                      fontSize: 26,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF211551),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )
+        ]),
+      ),
+    ));
+  }
+}
---
+++ lib/widgets.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class TaskCardWidget extends StatelessWidget {
+  final String title;
+  final String description;
+  TaskCardWidget({this.title, this.description});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      padding: EdgeInsets.symmetric(vertical: 32.0, horizontal: 24.0),
+      margin: EdgeInsets.only(bottom: 20.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            title ?? "(Unnamed Task)",
+            style: TextStyle(
+              color: Color(0xFF211551),
+              fontSize: 22.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 10.0),
+            child: Text(
+              description ?? "No Description Added",
+              style: TextStyle(
+                fontSize: 16.0,
+                color: Color(0xFF211511),
+                height: 1.5,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+//"Hello User! Welcomne to WHAT_TODO app, this is a default task that you can edit or delete to start using the app.",
